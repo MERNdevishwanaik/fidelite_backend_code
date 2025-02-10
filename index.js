52788d0afb916ea11af41d698935ec5b3fee21db
Tutorial-git <vishwa29naik@gmail.com>
2025-02-10 20:37:45 +0530

    updated index js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const connectDB = require("./config/db");
 const guestRoutes = require("./routes/guestRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
